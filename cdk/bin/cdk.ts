b73d7a93a9f5feef82918b7be0677282ac5fe5ad
Felix <23635466+its-felix@users.noreply.github.com>
2024-05-01 04:01:40 +0200

    add data stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Route53Stack } from '../lib/stacks/route53-stack';
 import { WebsiteStack } from '../lib/stacks/website-stack';
+import { DataStack } from '../lib/stacks/data-stack';
 
 const app = new cdk.App();
 
@@ -10,6 +11,8 @@ const websiteStack = new WebsiteStack(app, 'Website-Prod', {
   domain: 'explore.flights',
   certificateId: 'a96a703e-5454-4fc5-98eb-43b2f881be37',
 });
+
+new DataStack(app, 'Data-Prod', {});
 
 new Route53Stack(app, 'Route53-Prod', {
   zoneName: 'explore.flights',
---
+++ cdk/lib/stacks/data-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Bucket, BucketEncryption, IBucket } from 'aws-cdk-lib/aws-s3';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
+
+export interface DataStackProps extends cdk.StackProps {
+
+}
+
+export class DataStack extends cdk.Stack {
+  readonly bucket: IBucket;
+
+  constructor(scope: Construct, id: string, props: DataStackProps) {
+    super(scope, id, props);
+
+    this.bucket = new Bucket(this, 'DataBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE,
+      versioned: true,
+      lifecycleRules: [
+        {
+          enabled: true,
+          abortIncompleteMultipartUploadAfter: Duration.days(1),
+        },
+      ],
+    });
+  }
+}
