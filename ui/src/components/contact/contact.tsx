5877e32e586263fcfe7fd1ae0171b465ba507858
Felix <23635466+its-felix@users.noreply.github.com>
2024-05-12 00:24:16 +0200

    update contact email

--- ui/src/components/contact/contact.tsx
+++ ui/src/components/contact/contact.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Copy } from '../common/copy';
 import { KeyValuePairs, ValueWithLabel } from '../common/key-value-pairs';
 
+const EMAIL = 'contact@explore.flights';
+
 export function Contact() {
   return (
     <KeyValuePairs columns={1}>
       <ValueWithLabel label={'E-Mail'}>
-        <Box>Send us an E-Mail at <Copy copyText={'contact@gw2auth.com'}><Link href={'mailto:contact@gw2auth.com'} external={true}>contact@gw2auth.com</Link></Copy></Box>
+        <Box>Send us an E-Mail at <Copy copyText={EMAIL}><Link href={`mailto:${EMAIL}`} external={true}>{EMAIL}</Link></Copy></Box>
       </ValueWithLabel>
     </KeyValuePairs>
   );
