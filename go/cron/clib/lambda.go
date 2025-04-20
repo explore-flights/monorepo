cc529ce7566c962c700d807efa2c93099d5f3f16
Felix <23635466+its-felix@users.noreply.github.com>
2025-04-20 03:39:20 +0200

    fix duckdb extensions bundle

--- go/cron/clib/lambda.go
+++ go/cron/clib/lambda.go
@@ -2,15 +2,6 @@
 
 package clib
 
-import (
-	"os"
-)
-
 func DuckDBExtensionsPath(ddbHomePath string) string {
-	const path = "/opt/lib/duckdb_extensions"
-	if err := os.MkdirAll(path, 0750); err != nil {
-		panic(err)
-	}
-
-	return path
+	return "/opt/lib/duckdb_extensions"
 }
