c5c58335042a7f05326d1066d70a201b9cdfc90e
Felix <23635466+its-felix@users.noreply.github.com>
2025-06-19 02:23:29 +0200

    remove proxy code

--- go/proxy/config_local.go
+++
@@ -1,9 +0,0 @@
-//go:build !prod
-
-package main
-
-import "log/slog"
-
-func init() {
-	slog.SetLogLoggerLevel(slog.LevelDebug)
-}
--- go/proxy/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/explore-flights/monorepo/go/proxy
-
-go 1.23
